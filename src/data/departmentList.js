f13496e6d11ca3ff5ac2555d59af62a0f70ba614
tunsay <tuna1997@hotmail.fr>
2023-07-22 22:54:11 +0200

    add datas

---
+++ src/data/departmentList.js
@@ -0,0 +1,24 @@
+const departments = [
+  {
+    value: 'Sales',
+    label: 'Sales',
+  },
+  {
+    value: 'Marketing',
+    label: 'Marketing',
+  },
+  {
+    value: 'Engineering',
+    label: 'Engineering',
+  },
+  {
+    value: 'Human Resources',
+    label: 'Human Resources',
+  },
+  {
+    value: 'Legal',
+    label: 'Legal',
+  },
+]
+
+export default departments
