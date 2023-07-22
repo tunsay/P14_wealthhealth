a8ea8bbb4428d23fb3c51f5a548ff3d4cda1f664
tunsay <tuna1997@hotmail.fr>
2023-07-22 22:52:40 +0200

    add mocks for fake data

---
+++ src/_mock/mock.js
@@ -0,0 +1,13 @@
+const mockData = {
+  firstName: 'Momo',
+  lastName: 'Aktar',
+  dateofbirth: new Date().toLocaleDateString('fr-FR'),
+  startDate: new Date().toLocaleDateString('fr-FR'),
+  street: 'Alabama',
+  city: 'Vren',
+  state: 'Bro',
+  zipCode: '23455',
+  department: 'Lili',
+}
+
+export default mockData
