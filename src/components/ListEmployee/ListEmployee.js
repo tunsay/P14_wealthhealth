2db065f56a2ab8194e906306e85914dae5764ba8
tunsay <tuna1997@hotmail.fr>
2023-07-25 20:22:29 +0200

    class to classname

--- src/components/ListEmployee/ListEmployee.js
+++ src/components/ListEmployee/ListEmployee.js
@@ -67,7 +67,7 @@ export function ListEmployee() {
   )
 
   return (
-    <div id="employee-div" class="container">
+    <div id="employee-div" className="container">
       <h1>Current Employees</h1>
       <Link to={'/'}>View Current Employees</Link>
 
